676fc6f5cc11efa7aff5e75eae010167ca2fe8a8
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-04-07 13:02:56 +0300

    init

---
+++ FarkVar.psm1
@@ -0,0 +1,9 @@
+function Fark-Var {
+    $Diff = git.exe diff --color=always | Out-String
+    if ($Diff -ne "") {
+        $PlaySound = New-Object System.Media.SoundPlayer
+        $PlaySound.SoundLocation = ".\farkvar.wav"
+        $PlaySound.Play()
+        Write-Host $Diff
+    }
+}
